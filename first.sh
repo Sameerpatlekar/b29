404163e2106fb03be6af060d8c89509fc80f6a6d
root <root@ip-172-31-31-95.ec2.internal>
2024-04-12 10:46:47 +0000

    committed from ssh-instane

--- first.sh
+++ first.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-"""
-author : sameerpatlekar
-mail : sameer.gcp.gmail.com
-"""
+
+#author : sameerpatlekar
+#mail : sameer.gcp.gmail.com
+
+#this code written by git-ssh-instance
+first="sameer"
+last="patlekar"
+full="$first $last"
+
+echo "$full"
 
 
